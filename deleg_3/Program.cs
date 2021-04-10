6e22177a54456938b5d183ecb86883c0ab19ffbb
SirKotoff <68810705+SirKotoff@users.noreply.github.com>
2021-04-10 21:59:54 +0300

    Добавьте файлы проекта.

---
+++ deleg_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deleg_3
+{
+
+    public delegate void Deleg(object sender,EventArgs e);
+
+   
+
+    class Program
+    {
+         
+
+        public event Deleg P = null;
+ 
+
+
+        static void Main(string[] args)
+        {
+
+          
+
+
+        
+
+         }
+
+
+
+       
+
+
+
+
+
+
+
+    }
+}
